ab433979c2559cb46c8ae4f9773f7663e7836d10
NCrashed <NCrashed@gmail.com>
2014-11-17 14:22:12 +0300

    Calculating fitness, inital states of chromosomes and populations

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
+import Prelude as P
 import Data.Array.Repa hiding ((++))
 import Data.Functor
+import Data.List
+import qualified Data.Vector as Vec 
+import Control.Monad.Random
 import Text.Parsec
 import System.Environment (getArgs)
 import System.IO
@@ -54,6 +58,46 @@ saveResult path ts = withFile path WriteMode $ \h -> mapM_ (\(x,y) -> hPutStrLn 
 solve :: Task -> [Tower]
 solve (Task field towers radius) = undefined
 
+type Chromosome = Vec.Vector Bool
+type Population = [Chromosome]
+
+-- | Creating chromosome with random values, n is a length of chromosome
+initChromosome :: Int -> Rand StdGen Chromosome
+initChromosome n = Vec.replicateM n randBool
+  where randBool :: Rand StdGen Bool
+        randBool = liftRand $ random 
+
+-- | Creating population with m chromosomes with length n
+initPopulation :: Int -> Int -> Rand StdGen Population
+initPopulation m n = sequence $ replicate m $ initChromosome n
+
+filterTowers :: Chromosome -> [Tower] -> [Tower]
+filterTowers chr = P.map snd . filter ((== True) . fst) . zip (Vec.toList chr)
+
+toFloat :: Int -> Float
+toFloat = fromIntegral . toInteger
+
+-- | Calculates percentage of network coverage by solution in chromosome
+calcCoverage :: Chromosome -> Field -> Radius -> [Tower] -> Float
+calcCoverage chr field radius = coverage . foldl' placeTower field . filterTowers chr
+  where 
+    placeTower :: Field -> Tower -> Field 
+    placeTower f (tx, ty) = computeUnboxedS $ traverse f id $ \_ sh -> inRadius sh 
+      where
+        inRadius :: DIM2 -> Bool
+        inRadius (Z :. y :. x) = (tx-x)*(tx-x) + (ty-y)*(ty-y) <= radius*radius
+    coverage :: Field -> Float
+    coverage f = toFloat covered / toFloat area
+      where
+        covered = foldl' (\i b -> if b then i+1 else i) 0 (toList f)
+        area = size $ extent f
+
+-- | Calculates fitness for solution stored in chromosome
+fitness :: Chromosome -> Field -> Radius -> [Tower] -> Float
+fitness chr field radius twrs = (coverage + minimal) / 2.0
+  where coverage = calcCoverage chr field radius twrs
+        minimal = toFloat (length $ filterTowers chr twrs) / toFloat (length twrs)
+        
 -- | Reading input and output filenames from program arguments
 parseArgs :: IO (String, String)
 parseArgs = do
