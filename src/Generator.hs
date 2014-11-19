e393f3bb4e223df7fe5a2746da7cacaa040d06d2
NCrashed <ncrashed@gmail.com>
2014-11-19 03:01:17 +0300

    Generator executable

---
+++ src/Generator.hs
@@ -0,0 +1,56 @@
+module Generator where
+
+import Options.Applicative
+
+data Options = Options {
+    radiusMin :: Int
+  , radiusMax :: Int
+  , fieldWidth :: Int
+  , fieldHeight :: Int
+  , towersCount :: Int
+  , outputFile :: String
+}
+
+options :: Parser Options
+options = Options 
+  <$> option auto (
+       long "radiusMin"
+    <> metavar "value"
+    <> help "minimum radius of tower" 
+    <> value 3)
+  <*> option auto (
+       long "radiusMax"
+    <> metavar "value"
+    <> help "maximum radius of tower"
+    <> value 7)
+  <*> option auto (
+       long "fieldWidth"
+    <> short 'w'
+    <> metavar "WIDTH"
+    <> help "width of network field")
+  <*> option auto (
+       long "fieldHeight"
+    <> short 'h'
+    <> metavar "HEIGHT"
+    <> help "height of network field")
+  <*> option auto (
+       long "towersCount"
+    <> short 'n'
+    <> metavar "COUNT"
+    <> help "how many towers positions to generate")
+  <*> option auto (
+       long "output"
+    <> short 'o'
+    <> metavar "FILENAME"
+    <> help "where to save generated data set")
+  
+generator :: Options -> IO ()
+generator = undefined
+
+main :: IO ()
+main = execParser opts >>= generator
+  where
+    opts = info (helper <*> options) (
+         fullDesc
+      <> progDesc "Generates data set with COUNT towers and field of WIDTH x HEIGHT size"
+      <> header "radio-problem-generator - generator of data sets for 'radio-problem'" )
