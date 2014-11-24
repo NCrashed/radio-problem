aef72dc0750ad01cca3ecb4f26a00670847388ab
NCrashed <NCrashed@gmail.com>
2014-11-24 21:13:19 +0300

    Перевод на русский генератора условий

--- src/generator/Main.hs
+++ src/generator/Main.hs
@@ -5,6 +5,13 @@ import Control.Monad.Random
 import Control.Monad
 import System.IO
 
+import Data.Encoding
+import Data.Encoding.CP1251
+import Data.Encoding.UTF8
+
+e :: String -> String
+e = encodeString CP1251 . decodeString UTF8
+
 data Options = Options {
     radiusMin :: Int
   , radiusMax :: Int
@@ -19,41 +26,41 @@ options = Options
   <$> option auto (
        long "radiusMin"
     <> metavar "value"
-    <> help "minimum radius of tower" 
+    <> help (e "минимальный радиус башни")
     <> value 3)
   <*> option auto (
        long "radiusMax"
     <> metavar "value"
-    <> help "maximum radius of tower"
+    <> help (e "максимальный радиус башни")
     <> value 7)
   <*> option auto (
        long "fieldWidth"
     <> short 'w'
     <> metavar "WIDTH"
-    <> help "width of network field")
+    <> help (e "ширина поля сети"))
   <*> option auto (
        long "fieldHeight"
     <> short 'h'
     <> metavar "HEIGHT"
-    <> help "height of network field")
+    <> help (e "высота поля сети"))
   <*> option auto (
        long "towersCount"
     <> short 'n'
     <> metavar "COUNT"
-    <> help "how many towers positions to generate")
+    <> help (e "сколько генерировать возможных точек установки радио-вышек"))
   <*> strOption (
        long "output"
     <> short 'o'
     <> metavar "FILENAME"
-    <> help "where to save generated data set")
+    <> help (e "в какой файл сохранить результат генерации условия"))
     
 main :: IO ()
 main = execParser opts >>= generator
   where
     opts = info (helper <*> options) (
          fullDesc
-      <> progDesc "Generates data set with COUNT towers and field of WIDTH x HEIGHT size, stores output to FILENAME file location"
-      <> header "radio-problem-generator - generator of data sets for 'radio-problem'" )
+      <> progDesc (e "Создает входные данные для программы 'radio-problem' с количеством COUNT возможных точек установки радио-вышек и полем WIDTH на HEIGHT клеток, сохраняет вывод в файл с именем FILENAME")
+      <> header (e "radio-problem-generator - генератор входных данных для 'radio-problem'") )
 
 generator :: Options -> IO ()
 generator o = saveTask (outputFile o) =<< evalRandIO (generate o)
